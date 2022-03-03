d6d38acad6a19114feef99ea62e75eac6a21db74
bharatbhosle <bharatbhosle18@gmail.com>
2022-03-03 15:47:20 +0530

    1st commit...

--- src/Arithmatic/subtraction.java
+++ src/Arithmatic/subtraction.java
@@ -1,13 +1,13 @@
 package Arithmatic;
 
-public class Addition {
+public class subtraction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a,b,c;
-		a=20;
-		b=40;
-		c=a+b;
+		a=60;
+		b=20;
+		c=a-b;
 		System.out.println(c);
 
 	}
