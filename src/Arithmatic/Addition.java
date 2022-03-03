a63aa8d1cef53d9962860b7548006a00c159ad64
rahulkanade04 <rahulkanade04@gmail.com>
2022-03-03 11:42:07 +0530

    1st commit...

--- src/Arithmatic/Addition.java
+++ src/Arithmatic/Addition.java
@@ -4,6 +4,11 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println(c);
 
 	}
 
